bdbc763847e86c6bc85c4557bfb36fb466056d9c
aliffurqon <aqonlimited@gmail.com>
2021-01-21 15:43:47 +0700

    membuat styling pada component

--- App.js
+++ App.js
@@ -1,8 +1,86 @@
 import React, { Component } from 'react';
-import {Image, Text, TextInput, View} from 'react-native';
+import {Image, Text, TextInput, View, StyleSheet} from 'react-native';
+import macbook from './macbook.jpg';
 
 const App = () => {
-  return (
+  return <StylingReactNativeComponent />;
+  
+};
+
+const StylingReactNativeComponent = () => {
+  return(
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View style={{
+        width: 100, 
+        height: 100, 
+        backgroundColor: '#0abde3', 
+        borderWidth: 2,
+        borderColor: '#5f27cd',
+        marginTop: 20,
+        marginLeft:20,
+        }} />
+
+        <View style={{
+          padding: 12, 
+          backgroundColor: '#f2f2f2', 
+          width: 212, 
+          borderRadius: 8,
+        }} >
+          
+          <Image source={macbook} style={{width:188, height: 107, borderRadius: 8,}} />
+          
+          <Text style={{
+            fontSize:14, 
+            fontWeight: 'bold', 
+            marginTop: 16,
+          }}>New Macbook Pro 2019</Text>
+          
+          <Text style={{
+            fontSize: 12, 
+            fontWeight: 'bold', 
+            color: '#f2994a', 
+            marginTop: 12,
+          }}>Rp. 25.000.000</Text>
+
+          <Text style={{
+            fontSize:12, 
+            fontWeight: '300', 
+            marginTop: 12,
+          }}>Jakarta Barat</Text>
+
+          <View style={{
+            backgroundColor: '#6fcf97', 
+            borderRadius: 25, 
+            paddingVertical: 6, 
+            marginTop: 20,
+          }}>
+            
+            <Text style={{
+              fontSize:14, 
+              fontWeight:'600', 
+              color: 'white', 
+              textAlign: 'center',
+            }}>BELI</Text>
+          
+          </View>
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  text : {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#10ac84',
+    marginLeft: 20,
+    marginTop: 40,
+  }
+})
+
+const SampleComponent = () => {
+  return(
     <View>
       <View style={{width:80, height:80, backgroundColor: '#0abde3'}} />
       <Text>ALIF</Text>
@@ -14,7 +92,7 @@ const App = () => {
       <Profile />
     </View>
   );
-};
+}
 
 const Furqon = () => {
   return <Text>Furqon</Text>;
